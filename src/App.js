5790d2983e64ea6c2132a97bc9017a8a3c567f08
khaleel-lab <kbasha968@gmail.com>
2022-01-11 21:58:11 +0530

    Global Styles

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {
 	StyledButton,
 	SubmitButton,
 } from './components/Button/Button.styles';
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
 
 const theme = {
 	dark: {
@@ -16,11 +16,19 @@ const theme = {
 		primary: '#fff',
 		text: '#000',
 	},
+	fontFamily: 'Roboto',
 };
+
+const GlobalStyle = createGlobalStyle`
+button {
+	font-family: ${({ theme }) => theme.fontFamily};
+}
+`;
 
 function App() {
 	return (
 		<ThemeProvider theme={theme}>
+			<GlobalStyle />
 			<div className='App'>
 				{/* Animations  */}
 				<AnmiatedLogo src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/768px-React-icon.svg.png' />
