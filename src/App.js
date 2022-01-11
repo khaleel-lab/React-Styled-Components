8567f5a673d2431baa0eaa8739a5bdda93319716
khaleel-lab <kbasha968@gmail.com>
2022-01-11 18:25:29 +0530

    Basic Styled Component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { StyledButton } from './components/Button/Button.styles';
+
+function App() {
+	return (
+		<div className='App'>
+			<StyledButton>Styled Button</StyledButton>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Button/Button.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+	border: 2px solid #4caf50;
+	border-radius: 20px;
+	background-color: #4caf50;
+	color: #fff;
+	padding: 16px 32px;
+	margin: 16px auto;
+	text-align: center;
+	text-decoration: none;
+	display: block;
+	font-size: 16px;
+	cursor: pointer;
+	transition: 0.5s all ease-in-out;
+`;
+
+export default StyledButton;
